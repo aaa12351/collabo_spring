72d01e39b4c407a609918ca42f5cb439f2c884ce
lee <lee@example.com>
2025-09-18 17:08:09 +0900

    spring 2025/09/18

--- src/main/java/com/coffee/config/WebConfig.java
+++ src/main/java/com/coffee/config/WebConfig.java
@@ -1,11 +1,12 @@
 package com.coffee.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration // 해당 클래스를 객체로 만들어 주되, 이 파일은 설정용 파일입니다.
-
 public class WebConfig implements WebMvcConfigurer {
     // 자바에서 ...은 가변 매개 변수
     @Override // allowCredentials(true) : 브라우저가 서버와 통신할 때 쿠키/세션 등의 인증 정보를 주고 받는 것에 대하여 허락하겠습니다.
@@ -14,5 +15,16 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3000") /* react 포트 */
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") /* 허용할 메소드 */
                 .allowCredentials(true) ; // 쿠기 전송 허용
+    }
+
+    //application.properties 파일에서 uploadPath 항목의 값을 변수에 할당해 줍니다.
+    @Value("${uploadPath}")
+    private String uploadPath; //file:///C:/shop/images/
+
+    @Override //외부에서 "/images/**" 요청이 들어오면, uploadPath 를 한번 찾아 보세요.
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/images/**")
+                .addResourceLocations(uploadPath);
     }
 }
